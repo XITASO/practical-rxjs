fc61fc4d50c09ea06a35422e49e94c14f9d58e0a
Andreas Pfohl <andreas.pfohl@xitaso.com>
2017-12-07 15:31:37 +0100

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatFormField } from "@angular/material";
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatFormField
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
